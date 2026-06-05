d3d791c618b1ffdbd9ba623f49dc5b31943c7f44
zine-b <zineb.marricha99@gmail.com>
2026-06-05 14:09:17 +0200

    Replace SQLite with PostgreSQL

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,29 +1,31 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from dotenv import load_dotenv
-import os
-# -------------------------
-# Database configuration
-# -------------------------
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./cyber_soc.db")
 
-# l’objet qui gère la connexion entre app Python et la base de données
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+load_dotenv()
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./cyber_soc.db"
 )
 
-# Une session SQLAlchemy sert à parler avec la base de données : ajouter, lire, modifier, supprimer des données.
+engine_args = {}
+
+if DATABASE_URL.startswith("sqlite"):
+    engine_args["connect_args"] = {"check_same_thread": False}
+
+engine = create_engine(
+    DATABASE_URL,
+    **engine_args
+)
+
 SessionLocal = sessionmaker(
-    # les changements ne sont pas validés automatiquement --> db.commit()
-    # synchronise pas automatiquement les changements avec la base avant certaines requêtes
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# crée la classe de base pour définir tes tables.
 Base = declarative_base()
 
 def get_db():
